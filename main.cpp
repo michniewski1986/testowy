4091bde4031fe1428c2b7c83d03b0adbd33d4026
mat <michniewski@onet.pl>
2015-05-31 00:15:03 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <QCoreApplication>
-
+#include<QVector>
+#include<QDebug>
+#include<iostream>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+//    QVector<int> vec;
+//    qFill(vec,5);
+//    foreach (int val, vec) {
+//        std::cout << val;
+//    }
+
     // alleluja
+    int aa=5;
+        std::cout << aa;
     return a.exec();
 }
